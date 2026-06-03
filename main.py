6bebc3bea114c64c0fa40ccfe1f749b75e73e01c
MehradMi <165255856+MehradMi@users.noreply.github.com>
2026-06-03 10:00:40 +0330

    project base

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
+import os
+from dotenv import load_dotenv
+from openai import OpenAI
+
+load_dotenv()
+
 def main():
-    print("Hello from nyx-code!")
+	client = OpenAI(
+			base_url="https://api.gapgpt.app/v1",
+			api_key=os.environ.get("GAPGPT_API_KEY")
+	)
+      
+	response = client.chat.completions.create(
+			model="gemini-2.5-flash",
+			messages=[{"role": "user", "content": "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."}]
+	)
+	print(response.choices[0].message.content)
 
 
 if __name__ == "__main__":
