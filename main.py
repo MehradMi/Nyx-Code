662a435cffc5fc188fa6e6cc1ed5586d06ab9539
MehradMi <165255856+MehradMi@users.noreply.github.com>
2026-06-03 13:56:50 +0330

    added --verbose mode

--- main.py
+++ main.py
@@ -1,24 +1,36 @@
 import os
+import argparse
 from dotenv import load_dotenv
 from openai import OpenAI
 
 load_dotenv()
 
+GAPGPT_BASE_URL = "https://api.gapgpt.app/v1"
+OPENROUTER_BASE_URL = "https://openrouter.ai/api/v1"
+
 def main():
+		parser = argparse.ArgumentParser(description="Chatbot")
+		parser.add_argument("user_prompt", type=str, help="User prompt")
+		parser.add_argument("--verbose", action="store_true", help="Enable verbose output mode")
+		args = parser.parse_args()
+
 		client = OpenAI(
-			base_url="https://openrouter.ai/api/v1",
-			api_key=os.environ.get("OPENROUTER_NYX_CODE_KEY")
-		)
-		
-		model = "openrouter/owl-alpha"
+			base_url=GAPGPT_BASE_URL,
+			api_key=os.environ.get("GAPGPT_API_KEY")
+		)	
+		model = "gpt-4.1-nano"
+
+		messages = [{"role": "user", "content": args.user_prompt}]
 					
 		response = client.chat.completions.create(
 			model=model,
-			messages=[{"role": "user", "content": "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."}]
+			messages=messages
 		)
-		print('Prompt Tokens:', response.usage.prompt_tokens)
-		print('Completion Tokens:', response.usage.completion_tokens)
-		print('Total Tokens:', response.usage.total_tokens)
+		if args.verbose:
+			print('User Prompt:', args.user_prompt)	
+			print('Prompt Tokens:', response.usage.prompt_tokens)
+			print('Completion Tokens:', response.usage.completion_tokens)
+			print('Total Tokens:', response.usage.total_tokens)
 		print('Response:', response.choices[0].message.content)
 
 
